c1ff8abd869bf5102f6e22dc7bbe29c6fa38d8df
Sunrit Jana <warriordefenderz@gmail.com>
2022-12-08 23:04:20 +0530

    Added new fonts for the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Sen', 'sans-serif'],
+      title: ['Montserrat', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        primary: '#010101',
+        secondary: '#131313',
+        tertiary: '#222222',
+        accent: '#f3f3f3',
+        'accent-secondary': '#b5b5b5'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
