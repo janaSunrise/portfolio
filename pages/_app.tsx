237db14fd41023cf8e74651f725c0fda867ef78d
Sunrit Jana <warriordefenderz@gmail.com>
2022-12-08 23:13:50 +0530

    Get started with the real content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import '@fontsource/sen/'
+import '@fontsource/montserrat'
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 const Home = () => {
   return (
-    <div>Hello, World</div>
-  )
+    <div className="mx-12 md:mx-48 lg:mx-56 xl:mx-64">
+      <div className="mt-24">
+        <h2 className="text-2xl font-bold text-gray-400 font-title">
+          hey i am
+        </h2>
+        <h1 className="text-6xl font-bold text-accent font-title">
+          sunrit jana
+        </h1>
+      </div>
+
+      <p className="mt-6 text-lg font-medium text-accent-secondary">
+        sixteen. indian.
+        <br />
+        wizard, generalist, deviant, mostly water.
+        <br />
+        researches, makes weird stuff, and writes.
+      </p>
+    </div>
+  );
 }
 
 export default Home
